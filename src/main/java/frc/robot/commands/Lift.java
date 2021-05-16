dc62a82baa3075a3d0e7bf590a67aa0c73b094ab
richpants <rmansfield@creightonprep.org>
2021-05-16 11:21:46 -0500

    changed ...subsystem.Lift to ...subsystem.BallLift in import

--- src/main/java/frc/robot/commands/Lift.java
+++ src/main/java/frc/robot/commands/Lift.java
@@ -8,7 +8,7 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Constants;
 import frc.robot.RobotContainer;
-import frc.robot.subsystems.Lift;
+import frc.robot.subsystems.BallLift;
 
 public class Lift extends CommandBase {
   Lift lift;
